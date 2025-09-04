7754c1b68267e35b5882d90a1d3cb6edff388da6
Dorian Paprocki <dorianpaprocki@google.com>
2025-09-04 15:46:16 +0200

    review fixes

--- scripts/postprocess/active_video_ids.sql
+++ scripts/postprocess/active_video_ids.sql
@@ -1,19 +1,20 @@
 CREATE OR REPLACE TABLE `${raw_dataset}.active_video_ids`
 AS (
-    WITH active_assets as (
-        SELECT
-            PARSE_NUMERIC(SPLIT(`video_string`, '/')[SAFE_OFFSET(3)]) as asset_id
-        FROM
-            `{raw_dataset}.demand_gen_campaign_assets`,
-            UNNEST(ad_demand_gen_video_responsive_ad_videos) AS video_string
-        UNION DISTINCT SELECT
-            PARSE_NUMERIC( SPLIT(`ad_video_ad_video_asset`, '/')[SAFE_OFFSET(3)]) as asset_id
-        FROM `{raw_dataset}.video_campaigns`
-    )
+  WITH active_assets AS (
     SELECT
-        video_id
-    FROM `{raw_dataset}.video_assets`
-    WHERE
-        video_id IS NOT NULL AND
-        video_id NOT IN (SELECT video_id FROM `{raw_dataset}.video_aspect_ratios`) AND
-        asset_id IN (SELECT asset_id FROM active_assets) ORDER BY video_id)
+      PARSE_NUMERIC(SPLIT(`video_string`, '/')[SAFE_OFFSET(3)]) AS asset_id
+      FROM
+        `{raw_dataset}.demand_gen_campaign_assets`,
+        UNNEST(ad_demand_gen_video_responsive_ad_videos) AS video_string
+      UNION DISTINCT
+      SELECT
+        PARSE_NUMERIC( SPLIT(`ad_video_ad_video_asset`, '/')[SAFE_OFFSET(3)]) as asset_id
+      FROM `{raw_dataset}.video_campaigns`
+)
+  SELECT
+    video_id
+  FROM `{raw_dataset}.video_assets`
+  WHERE
+    video_id IS NOT NULL AND
+    video_id NOT IN (SELECT video_id FROM `{raw_dataset}.video_aspect_ratios`) AND
+    asset_id IN (SELECT asset_id FROM active_assets) ORDER BY video_id)
